1343155456202e0171f6087cfbd19c3e8044ad10
Chidambaram Somu <chidam.dev@gmail.com>
2018-06-21 04:13:06 +0000

    Ready for first release

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -22,6 +22,11 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'stock_quote', '~> 2.0'
+gem 'devise', '~> 4.4', '>= 4.4.3'
+gem 'descriptive_statistics', '~> 2.5', '>= 2.5.1'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -40,8 +45,13 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'sqlite3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+end
+
+group :production do
+  gem 'pg', '~> 1.0'
+  gem 'rails_12factor', '~> 0.0.3'
 end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,77 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    require 'mathn'
+    
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter the stock symbol"
+    elsif
+    
+      if params[:id]
+        begin
+          @url_delayedquote = 'https://api.iextrading.com/1.0/stock/' + params[:id] + '/quote'
+          @uri_delayedquote = URI(@url_delayedquote)
+          @response_delayedquote = Net::HTTP.get(@uri_delayedquote)
+          @stock_delayedquote = JSON.parse(@response_delayedquote)
+          
+          @url_companyinfo = 'https://api.iextrading.com/1.0/stock/' + params[:id] + '/company'
+          @uri_companyinfo = URI(@url_companyinfo)
+          @response_companyinfo = Net::HTTP.get(@uri_companyinfo)
+          @stock_companyinfo = JSON.parse(@response_companyinfo)
+          
+          @url_3mchart = 'https://api.iextrading.com/1.0/stock/' + params[:id] + '/chart/3m'
+          #https://api.iextrading.com/1.0/stock/aapl/chart/3m
+          @uri_3mchart = URI(@url_3mchart)
+          @response_3mchart = Net::HTTP.get(@uri_3mchart)
+          @stock_3mchart = JSON.parse(@response_3mchart)
+          
+          
+          @dayscount = @stock_3mchart.count
+          @arrayitemnumberlastchange = @dayscount - 1
+          @arrayitemnumber = @dayscount - 2
+          @rsiperiod = 14
+          @rsiperiodsmooth = @rsiperiod-1
+          
+          @rollingpositive = 0
+          @numberofdayspositive = 0
+          @rollingnegative = 0
+          @numberofdaysnegative = 0
+          @i = 0
+          @lastpositivechange = 0
+          @lastnegativechange = 0
+          
+          if @stock_3mchart[@arrayitemnumberlastchange]['change'].to_f > 0
+            @lastpositivechange = @stock_3mchart[@arrayitemnumberlastchange]['change'].to_f
+          elsif @stock_3mchart[@arrayitemnumberlastchange]['change'].to_f <= 0
+            @lastnegativechange = @stock_3mchart[@arrayitemnumberlastchange]['change'].to_f
+          end
+            
+         
+          while @i < @rsiperiodsmooth do
+            @stock_3mchartnumber = @stock_3mchart[@arrayitemnumber]['change'].to_f 
+            if @stock_3mchartnumber > 0
+              @rollingpositive = @rollingpositive + @stock_3mchartnumber
+              @numberofdayspositive = @numberofdayspositive + 1
+            elsif @stock_3mchartnumber < 0
+              @rollingnegative = @rollingnegative + @stock_3mchartnumber
+              @numberofdaysnegative = @numberofdaysnegative + 1
+            end
+            @arrayitemnumber -= 1
+            @i += 1
+          end
+          
+          @postiveaverage = ((1/@rsiperiod) * @lastpositivechange) + (@rollingpositive * (@rsiperiodsmooth/@rsiperiod))
+          @negativeaverage = ((1/@rsiperiod) * @lastnegativechange) + (@rollingnegative * (@rsiperiodsmooth/@rsiperiod))
+          
+          @avggainloss3m = @postiveaverage/(-1 * @negativeaverage)
+          @rsi14 = (100 - (100/(1+@avggainloss3m))).round(2) 
+        
+        rescue StandardError
+          @error = "That Stock Symbol Doesn't Exist."
+        end
+      end 
+    end
   end
   
   def about
